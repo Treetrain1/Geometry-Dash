0b979970aca2976b308e19d4ffc2ff3fd658e2de
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-01-15 22:00:22 -0500

    fix rot axis

--- src/main/java/me/treetrain1/geometrydash/entity/render/model/CubePlayerModel.java
+++ src/main/java/me/treetrain1/geometrydash/entity/render/model/CubePlayerModel.java
@@ -53,8 +53,8 @@ public class CubePlayerModel<T extends AbstractClientPlayer> extends Hierarchica
 	@Override
 	public void renderToBuffer(@NotNull PoseStack poseStack, @NotNull VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
 		poseStack.pushPose();
-		poseStack.translate(0D, 1.3D, 0D);
-		poseStack.mulPose(Axis.ZP.rotation(this.cubeRotation));
+		poseStack.translate(0D, 1.3D + 0.2375D, 0D);
+		poseStack.mulPose(Axis.XP.rotation(this.cubeRotation));
 		this.root().render(poseStack, vertexConsumer, packedLight, OverlayTexture.NO_OVERLAY, red, green, blue, alpha);
 		poseStack.popPose();
 	}
