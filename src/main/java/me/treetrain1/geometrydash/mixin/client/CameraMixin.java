e849ed418b57618f8395dba2b55a5b4bc89cc190
AViewFromTheTop <87103914+AViewFromTheTop@users.noreply.github.com>
2024-01-18 00:37:11 -0500

    better mixin maybe idk

--- src/main/java/me/treetrain1/geometrydash/mixin/client/CameraMixin.java
+++ src/main/java/me/treetrain1/geometrydash/mixin/client/CameraMixin.java
@@ -1,5 +1,9 @@
 package me.treetrain1.geometrydash.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
 import me.treetrain1.geometrydash.duck.PlayerDuck;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -17,24 +21,63 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class CameraMixin {
 
 	@Shadow
-	protected abstract void setRotation(float yRot, float xRot);
-
-	@Shadow
-	public abstract float getYRot();
-
-	@Shadow
-	public abstract float getXRot();
-
-	@Shadow
 	protected abstract void move(double distanceOffset, double verticalOffset, double horizontalOffset);
 
-	@Inject(method = "setup", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;setPosition(DDD)V", shift = At.Shift.AFTER), cancellable = true)
-	private void setupGD(BlockGetter level, Entity entity, boolean detached, boolean thirdPersonReverse, float partialTick, CallbackInfo ci) {
-		if (!(entity instanceof PlayerDuck duck) || !duck.geometryDash$getGDData().getPlayingGD()) return;
+	@WrapOperation(
+		method = "setup",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/Camera;setRotation(FF)V",
+			ordinal = 0
+		)
+	)
+	private void gd$setupRotation(
+		Camera instance, float yRot, float xRot, Operation<Void> original,
+		BlockGetter level, Entity entity, boolean detached, boolean thirdPersonReverse, float partialTick,
+		@Share("gd$isGD") LocalBooleanRef isGD
+	) {
+		if (entity instanceof PlayerDuck duck && duck.geometryDash$getGDData().getPlayingGD()) {
+			yRot += 270F;
+			xRot = 0;
+			isGD.set(true);
+		}
 
-		this.move(0, -1.137, -10);
-		this.setRotation(this.getYRot() + 270.0F, 0F);
+		original.call(instance, yRot, xRot);
+	}
 
-		ci.cancel();
+	@WrapOperation(
+		method = "setup",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/Camera;setPosition(DDD)V",
+			ordinal = 0
+		)
+	)
+	private void gd$setupPosition(
+		Camera instance, double x, double y, double z, Operation<Void> original,
+		BlockGetter level, Entity entity, boolean detached, boolean thirdPersonReverse, float partialTick,
+		@Share("gd$isGD") LocalBooleanRef isGD
+	) {
+		original.call(instance, x, y, z);
+		if (!isGD.get()) return;
+
+		this.move(0D, -1.137D, -10D);
+	}
+
+	@Inject(
+		method = "setup",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/Camera;setPosition(DDD)V",
+			ordinal = 0,
+			shift = At.Shift.AFTER
+		),
+		cancellable = true
+	)
+	private void gd$cancelThirdPerson(
+		BlockGetter level, Entity entity, boolean detached, boolean thirdPersonReverse, float partialTick, CallbackInfo ci,
+		@Share("gd$isGD") LocalBooleanRef isGD
+	) {
+		if (isGD.get()) ci.cancel();
 	}
 }
