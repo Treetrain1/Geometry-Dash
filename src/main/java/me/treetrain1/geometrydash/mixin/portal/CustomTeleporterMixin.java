bd86c23caa588594e232fbef9e6ccad022b3aad6
Treetrain1 <treetrain1@gmail.com>
2024-02-27 22:14:35 -0600

    dimension spawn constant

--- src/main/java/me/treetrain1/geometrydash/mixin/portal/CustomTeleporterMixin.java
+++ src/main/java/me/treetrain1/geometrydash/mixin/portal/CustomTeleporterMixin.java
@@ -3,6 +3,7 @@ package me.treetrain1.geometrydash.mixin.portal;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.treetrain1.geometrydash.GeometryDash;
+import me.treetrain1.geometrydash.util.GDSharedConstantsKt;
 import net.kyrptonaught.customportalapi.util.CustomTeleporter;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
@@ -15,7 +16,8 @@ public class CustomTeleporterMixin {
 	@WrapOperation(method = "createDestinationPortal", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/BlockPos;containing(DDD)Lnet/minecraft/core/BlockPos;"))
 	private static BlockPos setGDPos(double x, double y, double z, Operation<BlockPos> original, ServerLevel destination) {
 		if (destination.dimension() == GeometryDash.DIMENSION) {
-			return original.call(0.0, 10.0, 0.0);
+			var spawn = GDSharedConstantsKt.DIMENSION_SPAWN;
+			return original.call(spawn.x, spawn.y, spawn.z);
 		}
 		return original.call(x, y, z);
 	}
--- src/main/java/me/treetrain1/geometrydash/util/GDSharedConstants.kt
+++ src/main/java/me/treetrain1/geometrydash/util/GDSharedConstants.kt
@@ -1,6 +1,8 @@
 package me.treetrain1.geometrydash.util
 
+import net.minecraft.core.BlockPos
 import net.minecraft.world.entity.ai.attributes.AttributeModifier
+import net.minecraft.world.phys.Vec3
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -22,3 +24,9 @@ val GD_MOVEMENT_SPEED = AttributeModifier(
     AttributeModifier.Operation.MULTIPLY_TOTAL
 )
 const val GD_GRAVITY_PULL: Double = -0.1//-0.18
+
+/**
+ * The location where portals to the Geometry dimension will lead.
+ */
+@JvmField
+val DIMENSION_SPAWN = Vec3(0.0, 10.0, 0.0)
