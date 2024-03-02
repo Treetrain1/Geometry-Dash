b68986fbeb9750e48301b036d63a22bd047bad0c
Treetrain1 <treetrain1@gmail.com>
2024-03-01 23:03:28 -0600

    large spike model + texture

--- src/main/java/me/treetrain1/geometrydash/block/SpikeBlock.kt
+++ src/main/java/me/treetrain1/geometrydash/block/SpikeBlock.kt
@@ -12,7 +12,6 @@ import net.minecraft.world.level.Level
 import net.minecraft.world.level.LevelAccessor
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Fallable
-import net.minecraft.world.level.block.PointedDripstoneBlock
 import net.minecraft.world.level.block.SimpleWaterloggedBlock
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
