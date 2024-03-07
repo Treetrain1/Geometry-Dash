9dad4be6ed2da8aad72d05541a51faac55d3aa5a
Treetrain1 <treetrain1@gmail.com>
2024-03-06 19:02:29 -0600

    add music pausing, resuming, and closing

--- src/main/java/me/treetrain1/geometrydash/mixin/client/MinecraftMixin.java
+++ src/main/java/me/treetrain1/geometrydash/mixin/client/MinecraftMixin.java
@@ -3,16 +3,47 @@ package me.treetrain1.geometrydash.mixin.client;
 import de.keksuccino.melody.resources.audio.openal.ALAudioClip;
 import me.treetrain1.geometrydash.duck.MCDuck;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.multiplayer.ClientLevel;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin implements MCDuck {
 
+	@Shadow
+	@Nullable
+	public Screen screen;
+	@Shadow
+	@Nullable
+	public ClientLevel level;
 	@Unique
 	@Nullable
 	private ALAudioClip audioClip = null;
+
+	@Inject(method = "runTick", at = @At("TAIL"))
+	private void controlPause(boolean renderLevel, CallbackInfo ci) {
+		var audioClip = this.audioClip;
+		if (audioClip != null) {
+			try {
+				if (this.screen == null) {
+					audioClip.resume();
+				} else {
+					if (this.level == null) {
+						audioClip.stop();
+						audioClip.closeQuietly();
+						this.audioClip = null;
+					} else audioClip.pause();
+				}
+			} catch (Exception ignored) {}
+		}
+	}
 
 	@Nullable
 	@Override
